2aa0909730db0f55751ca3aa96e888724d599cdb
Matheus Chagas <matheusssbbb539@gmail.com>
2024-12-01 18:01:59 -0300

    adjustments

--- database/trigger.sql
+++ database/trigger.sql
@@ -6,19 +6,19 @@ CREATE TRIGGER atualizar_relatorio_entrada_saida
 AFTER INSERT ON CATRACA
 FOR EACH ROW
 BEGIN
-    DECLARE tempo_total TIME;
-    DECLARE tempo_semanal TIME;
-    DECLARE classificacao_total VARCHAR(50);
-    DECLARE classificacao_semanal VARCHAR(50);
+    DECLARE tempo_total TIME DEFAULT '00:00:00';
+    DECLARE tempo_semanal TIME DEFAULT '00:00:00';
+    DECLARE classificacao_total VARCHAR(50) DEFAULT 'Iniciante';
+    DECLARE classificacao_semanal VARCHAR(50) DEFAULT 'Iniciante';
 
     -- Calculando as horas totais
-    SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(HORARIO_SAIDA, HORARIO_INICIO)))) 
+    SELECT SEC_TO_TIME(IFNULL(SUM(TIME_TO_SEC(TIMEDIFF(HORARIO_SAIDA, HORARIO_INICIO))), 0)) 
     INTO tempo_total
     FROM CATRACA
     WHERE ID_ALUNO = NEW.ID_ALUNO AND HORARIO_SAIDA IS NOT NULL;
 
     -- Calculando as horas semanais (últimos 7 dias)
-    SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(TIMEDIFF(HORARIO_SAIDA, HORARIO_INICIO)))) 
+    SELECT SEC_TO_TIME(IFNULL(SUM(TIME_TO_SEC(TIMEDIFF(HORARIO_SAIDA, HORARIO_INICIO))), 0)) 
     INTO tempo_semanal
     FROM CATRACA
     WHERE ID_ALUNO = NEW.ID_ALUNO 
@@ -27,24 +27,24 @@ BEGIN
 
     -- Classificação de horas totais
     IF TIME_TO_SEC(tempo_total) <= 18000 THEN
-        SET classificacao_total = 'Iniciante';  -- Até 5 horas (18000 segundos)
+        SET classificacao_total = 'Iniciante';
     ELSEIF TIME_TO_SEC(tempo_total) > 18000 AND TIME_TO_SEC(tempo_total) <= 36000 THEN
-        SET classificacao_total = 'Intermediário';  -- 5 a 10 horas (18000-36000 segundos)
+        SET classificacao_total = 'Intermediário';
     ELSEIF TIME_TO_SEC(tempo_total) > 36000 AND TIME_TO_SEC(tempo_total) <= 72000 THEN
-        SET classificacao_total = 'Avançado';  -- 10 a 20 horas (36000-72000 segundos)
+        SET classificacao_total = 'Avançado';
     ELSE
-        SET classificacao_total = 'Extremamente Avançado';  -- Mais de 20 horas
+        SET classificacao_total = 'Extremamente Avançado';
     END IF;
 
     -- Classificação de horas semanais
     IF TIME_TO_SEC(tempo_semanal) <= 18000 THEN
-        SET classificacao_semanal = 'Iniciante';  -- Até 5 horas (18000 segundos)
+        SET classificacao_semanal = 'Iniciante';
     ELSEIF TIME_TO_SEC(tempo_semanal) > 18000 AND TIME_TO_SEC(tempo_semanal) <= 36000 THEN
-        SET classificacao_semanal = 'Intermediário';  -- 5 a 10 horas (18000-36000 segundos)
+        SET classificacao_semanal = 'Intermediário';
     ELSEIF TIME_TO_SEC(tempo_semanal) > 36000 AND TIME_TO_SEC(tempo_semanal) <= 72000 THEN
-        SET classificacao_semanal = 'Avançado';  -- 10 a 20 horas (36000-72000 segundos)
+        SET classificacao_semanal = 'Avançado';
     ELSE
-        SET classificacao_semanal = 'Extremamente Avançado';  -- Mais de 20 horas
+        SET classificacao_semanal = 'Extremamente Avançado';
     END IF;
 
     -- Atualizando a tabela RELATORIO
